4774c0d7d4c1cb73c5f2f73e75a7335afff1b686
Hardi Surya Pratama <hardisur@Hardis-MacBook.local>
2026-03-06 13:00:30 +0700

    fix: update base path untuk repo baru MKPbersih

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/sosialisasimkpbersihraker/',
+  base: '/MKPbersih/',
   build: {
     rollupOptions: {
       input: {
